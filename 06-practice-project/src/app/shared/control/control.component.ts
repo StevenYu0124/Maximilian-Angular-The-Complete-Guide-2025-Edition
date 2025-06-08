e577db6f8c279b7349feedcf801504cbacfa8d08
Steven Yu <stevenyu@Stevens-MacBook-Air.local>
2025-06-08 14:51:01 +0800

    123. Implementing Lifecycle Interfaces

--- 06-practice-project/src/app/shared/control/control.component.ts
+++ 06-practice-project/src/app/shared/control/control.component.ts
@@ -1,12 +1,22 @@
-import { Component, input } from '@angular/core';
+import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
   label = input<string>();
+  private el = inject(ElementRef);
+  onClick(): void {
+    console.log('Control clicked:', this.label());
+    console.log(this.el);
+  }
 }
